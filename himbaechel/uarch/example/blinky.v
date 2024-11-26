32df591de8746b8c2293450e8dddee3d59c1189a
Kelvin Chung <king.chung@manchester.ac.uk>
2024-11-26 12:20:41 +0000

    drawing

--- himbaechel/uarch/example/blinky.v
+++ himbaechel/uarch/example/blinky.v
@@ -1,14 +1,11 @@
-module top(input rst, output reg [7:0] leds);
+module top(input rst, clk, output reg [7:0] leds);
 
-wire clk;
+// wire clk;
 
-(* BEL="X1Y0/IO0" *) INBUF ib_i (.O(clk));
+// (* BEL="X1Y0/IO0" *) INBUF ib_i (.O(clk));
 
 reg [7:0] ctr;
 always @(posedge clk)
-	if (rst)
-		ctr <= 8'h00;
-	else
 		ctr <= ctr + 1'b1;
 
 assign leds = {4'b1010, ctr[7:4]};
