d480ac1664e00e2a6ab94f8c9af75e525443ecc9
Kelvin Chung <king.chung@manchester.ac.uk>
2024-11-29 00:07:30 +0000

    time multiplex

--- himbaechel/uarch/FABulous/FABulous_utils.h
+++ himbaechel/uarch/FABulous/FABulous_utils.h
@@ -2,7 +2,6 @@
 #define FABULOUS_UTILS_H
 
 #include "design_utils.h"
-#include "idstringlist.h"
 #include "nextpnr_namespaces.h"
 #include "nextpnr_types.h"
 
@@ -18,6 +17,8 @@ struct FABulousUtils {
     int get_context_count();
     int get_real_bel_count();
     int get_bel_context(BelId bel) const;
+    dict<IdString, std::vector<IdString>> get_tile_unique_bel_type() const;
+    IdString get_tile_type(int x, int y) const;
 };
 
 NEXTPNR_NAMESPACE_END
